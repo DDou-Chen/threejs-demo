84cc739c7de2871e4d43215c13e0539a6cd0ef68
兜兜 <913321406@qq.com>
2025-05-30 10:11:20 +0800

    简单3d渲染

---
+++ index.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// 创建了一个 Scene
+const scene = new THREE.Scene();
+
+console.log(THREE);
+
+{
+  // BoxGeometry 立方体
+  const geometry = new THREE.BoxGeometry(100, 100, 100);
+  // 漫反射材质 MeshLambertMaterial
+  const material = new THREE.MeshLambertMaterial({
+    color: new THREE.Color("orange"),
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  // 添加物体 Mesh
+  mesh.position.set(0, 0, 0);
+  scene.add(mesh);
+}
+
+// 添加灯光 Light
+{
+  const pointLight = new THREE.PointLight(0xffffff, 10000);
+  pointLight.position.set(80, 80, 80);
+  scene.add(pointLight);
+}
+
+// 坐标系的工具 AxesHelper
+{
+  const axesHelper = new THREE.AxesHelper(200);
+  scene.add(axesHelper);
+}
+
+{
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  // 透视相机 角度 宽高比 最近是哪 最远是哪
+  const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+  // 在 200,200,200 的位置看向 0,0,0
+  camera.position.set(200, 200, 200);
+  camera.lookAt(0, 0, 0);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+
+  function render() {
+    renderer.render(scene, camera);
+    // 把 render 改成渲染循环，用 requestAnimationFrame 来一帧帧的循环渲染
+    requestAnimationFrame(render);
+  }
+
+  render();
+
+  document.body.append(renderer.domElement);
+
+  // 创建 OrbitControls 的实例，传入 camera 和 canvas 元素
+  const controls = new OrbitControls(camera, renderer.domElement);
+}
